5497a1f03be8424caf20cd4f6f2b1f99ba6c8aac
Blnum1 <jirayut.pr@ku.th>
2024-09-10 15:48:15 +0700

    login

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  login(): void {
+    if (this.loginForm.valid) {
+      // Handle login logic here
+      console.log(this.loginForm.value);
+      // You can add further logic here to handle login
+    }
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { MenuComponent } from './menu/menu.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ReservationComponent } from './reservation/reservation.component';
+import { LoginComponent  } from './admin/login/login.component';
+import { ManageComponent } from './admin/manage/manage.component';
+
+
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' }, 
@@ -12,7 +17,10 @@ const routes: Routes = [
   { path: 'menu', component: MenuComponent },
   { path: 'about-us', component: AboutUsComponent }, 
   { path: 'contact-us', component: ContactUsComponent },
-  { path: 'reservation', component: ReservationComponent }
+  { path: 'reservation', component: ReservationComponent },
+  { path: 'admin/login', component: LoginComponent },
+  { path: 'admin/manage', component: ManageComponent },
+  { path: 'admin', redirectTo: 'admin/admin-login', pathMatch: 'full' }
 ];
 
 @NgModule({
